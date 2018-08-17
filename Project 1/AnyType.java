38413a0a42965f97bd64fe50f551f9c33f3d192c
Matthew Krieger <mattkrieger3@gmail.com>
2018-08-17 13:52:08 -0400

    Create AnyType.java

---
+++ Project 1/AnyType.java
@@ -0,0 +1,12 @@
+/* 		Created By: 		Matthew A. Krieger
+ * 		Date:			October 04, 2015
+ * 		Assignment:		Homework II	
+ */
+
+import java.util.Scanner;
+
+interface AnyType 
+{
+   public boolean isBetterThan(AnyType datum);
+   public boolean isLessThan(AnyType datum);
+}
