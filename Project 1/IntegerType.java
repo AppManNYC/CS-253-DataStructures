12d99af882ad9d1e14bb3aa0f8c6041d591d2af7
Matthew Krieger <mattkrieger3@gmail.com>
2018-08-17 13:53:45 -0400

    Create IntegerType.java

---
+++ Project 1/IntegerType.java
@@ -0,0 +1,31 @@
+/* 		Created By: 		Matthew A. Krieger
+ * 		Date:			October 04, 2015
+ * 		Assignment:		Homework II
+ */
+
+class IntegerType implements AnyType {
+
+   private int number;
+
+   public IntegerType() {
+       number = 0;
+   }
+   
+   public IntegerType(int i) { 
+       number = i;
+   }
+
+   public boolean isBetterThan(AnyType datum) {
+	return (this.number > ((IntegerType)datum).number);
+   }
+   
+   public boolean isLessThan(AnyType datum) {
+	return (this.number < ((IntegerType)datum).number);
+   }
+
+   public int toInteger() {
+	return number;
+   }
+}
+
+
