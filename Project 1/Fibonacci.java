739372025ad7ec4ca51415cb31806938c164a407
Matthew Krieger <mattkrieger3@gmail.com>
2018-08-17 13:51:23 -0400

    Create Fibonacci.java

---
+++ Project 1/Fibonacci.java
@@ -0,0 +1,44 @@
+/*      Created By:         Matthew A. Krieger
+ *      Date:               October 04, 2015
+ *      Assignment:         Homework II  
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class Fibonacci
+{
+    public static void main (String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        int n = 0, fib;
+        while (n != -1)
+        {
+            System.out.println("Enter N: ");
+            n = scan.nextInt();
+            if (n == -1)
+            {
+                System.out.println("Program Finished.");
+            }
+            else
+            {
+                fib = Fibonacci(n);
+                System.out.println(fib);
+                System.out.println();
+            }
+        }
+
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n<=1)
+        {
+            return n;
+        }
+        else 
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
